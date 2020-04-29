530980f238f080edf0c89b71669f56c2cecaf8c8
XGQT <xgqt@protonmail.com>
2020-04-30 00:26:31 +0200

    update install script add '{}' and just back up yhe existing user.js

--- install.sh
+++ install.sh
@@ -4,45 +4,34 @@
 # Install user.js
 
 
-firefox_dir="$HOME/.mozilla/firefox"
+firefox_dir="${HOME}/.mozilla/firefox"
 
 
-for profile in "$firefox_dir"/*
+for profile in "${firefox_dir}"/*
 do
-	if [ -d "$profile" ]
-	then
+    if [ -d "${profile}" ]
+    then
 
-		echo "Found: $profile"
-		echo "Copy user.js there? [y/n]"
+        echo "Found: ${profile}"
+        echo "Copy user.js there? [y/n]"
 
-		read -r answer
-
-		case $answer
-		in
-			[Yy]* )
-
-				if [ -f "$profile/user.js" ]
-				then
-					echo "Diff:"
-					diff user.js "$profile/user.js"
-				else
-					echo "There was no user.js in $profile"
-				fi
-				
-				cp user.js "$profile" && echo "Copied"
-
-				;;
-			[Nn]* )
-
-				echo "Skipping: $profile"
-
-				;;
-			* )
-
-				echo "I will take that as a 'No'"
-				echo "Skipping: $profile"
-
-				;;
-		esac
-	fi
+        read -r answer
+        case ${answer} in
+            [Yy]* )
+                if [ -f "${profile}/user.js" ]
+                then
+                    echo "Backing up existing user.js"
+                    mv "${profile}/user.js" "${profile}/user.js.bak"
+                fi
+                cp user.js "${profile}" && echo "Done: ${profile}"
+                ;;
+            [Nn]* )
+                echo "Skipping: ${profile}"
+                ;;
+            * )
+                echo "I will take that as a 'No'"
+                echo "Skipping: ${profile}"
+                ;;
+        esac
+    fi
 done
